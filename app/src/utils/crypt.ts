3a94c49d51a7643931f98d7a9b8f012f648bbdf6
Snuffish <snuffish90@gmail.com>
2024-01-18 14:54:48 +0100

    Update crypt.ts

--- app/src/utils/crypt.ts
+++ app/src/utils/crypt.ts
@@ -1,11 +1,25 @@
 import { Wallet } from 'ethers';
 import { HmacSHA256, enc } from 'crypto-js';
+import * as sigUtil from "@metamask/eth-sig-util";
+import * as ethUtil from "ethereumjs-util";
 
 export const encryptInternalData = (wallet: Wallet, data: string) => {
     const encrypted = HmacSHA256(wallet.address, wallet.privateKey)
     const encHash = encrypted.toString(enc.Hex)
 
     return encHash
+}
+
+const encrypt = (publicKey: string, text: string) => {
+    const result = sigUtil.encrypt({
+        publicKey,
+        data: text,
+        // https://github.com/MetaMask/eth-sig-util/blob/v4.0.0/src/encryption.ts#L40
+        version: 'x25519-xsalsa20-poly1305'
+    });
+    
+    // https://docs.metamask.io/guide/rpc-api.html#other-rpc-methods
+    return ethUtil.bufferToHex(Buffer.from(JSON.stringify(result), 'utf8'))
 }
 
 // export const internalDecryptDawta = (wallet: Wallet, hash: string) => {
