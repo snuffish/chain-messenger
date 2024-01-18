533579c038caa12ac922bcc0bfc405f46175147c
Snuffish <snuffish90@gmail.com>
2024-01-18 14:57:50 +0100

    Update crypt.ts

--- app/src/utils/crypt.ts
+++ app/src/utils/crypt.ts
@@ -2,15 +2,21 @@ import { Wallet } from 'ethers';
 import { HmacSHA256, enc } from 'crypto-js';
 import * as sigUtil from "@metamask/eth-sig-util";
 import * as ethUtil from "ethereumjs-util";
+import { useMetamask } from "use-metamask";
+import { providers } from "ethers";
 
 export const encryptInternalData = (wallet: Wallet, data: string) => {
-    const encrypted = HmacSHA256(wallet.address, wallet.privateKey)
+    const encrypted = HmacSHA256(data, wallet.privateKey)
     const encHash = encrypted.toString(enc.Hex)
 
     return encHash
 }
 
-const encrypt = (publicKey: string, text: string) => {
+const requestPublicKey = (web3: providers.Web3Provider, account: string) => {
+  return web3.send("eth_getEncryptionPublicKey", [account]);
+};
+
+const ethEncrypt = (publicKey: string, text: string) => {
     const result = sigUtil.encrypt({
         publicKey,
         data: text,
@@ -21,6 +27,15 @@ const encrypt = (publicKey: string, text: string) => {
     // https://docs.metamask.io/guide/rpc-api.html#other-rpc-methods
     return ethUtil.bufferToHex(Buffer.from(JSON.stringify(result), 'utf8'))
 }
+
+const ethDecrypt = async (
+    web3: providers.Web3Provider,
+    account: string,
+    text: string
+) => {
+    const result = await web3.send("eth_decrypt", [text, account])
+    return result
+};
 
 // export const internalDecryptDawta = (wallet: Wallet, hash: string) => {
 //     const internalHash = internalEncryptData(wallet, hash);
