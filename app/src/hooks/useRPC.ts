56aea80f8cdeb81ec20687d7567bf56f57956611
Snuffish <snuffish90@gmail.com>
2024-01-27 10:05:56 +0100

    wip

--- app/src/hooks/useRPC.ts
+++ app/src/hooks/useRPC.ts
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 import { parseHexToString } from "../utils"
+import { createPublicClient, http } from "viem"
+import { localhost } from "viem/chains"
 
 type Props = {
     method: string,
@@ -7,11 +9,18 @@ type Props = {
 }
 
 const useRPC = ({ method, params = [] }: Props) => {
-    const body = {
-        "jsonrpc": "2.0",
-        method,
-        params
-    }
+    // const client = createPublicClient({
+    //     chain: localhost,
+    //     transport: http('https://a67f-193-181-34-151.ngrok-free.app')
+    // })
+
+    // client.getBlockNumber().then(res => console.log("RES => ", res)).catch(err => console.log("ERR => ", err))
+
+    // const body = {
+    //     "jsonrpc": "2.0",
+    //     method,
+    //     params
+    // }
     // const body = { "jsonrpc": "2.0", "method": "eth_getBalance", "params": ["0xD0B7615f7CefbcF959cD5Ea58E3bC88C533c8d0b"], "id": 83 }
 
     // const { data, error, isError } = useQuery([], {
