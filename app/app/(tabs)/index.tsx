5658a5c64c2747918844c674d2c723c6281d38ed
Snuffish <snuffish90@gmail.com>
2024-02-10 23:00:29 +0100

    wip

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from 'tamagui'
+import React from 'react'
+import { Avatar, Text, View } from 'tamagui'
+
+type UserRowTypes = {
+  username: string
+}
+const UserRow = ({ username }: UserRowTypes) => {
+  return (
+    <Avatar circular size='$5'>
+      <Avatar.Image accessibilityLabel='dsdsdsd' src="https://avataaars.io/?avatarStyle=Circle&topType=NoHair&accessoriesType=Round&facialHairType=BeardMedium&facialHairColor=Red&clotheType=BlazerSweater&eyeType=WinkWacky&eyebrowType=UpDown&mouthType=Vomit&skinColor=Light" />
+    </Avatar>
+  )
+}
 
 const Messages = () => {
   return (
     <View>
-      <Text>MESSAGES</Text>
+      <UserRow username='Snuffish' />
     </View>
   )
 }
---
+++ app/app/(tabs)/messages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, SizeTokens, Spacer, Text, View, YStack, createStyledContext, styled } from 'tamagui'
+
+const Settings = (): any => {
+  return (
+    <View>
+      <Text>DSDSDS</Text>
+    </View>
+  )
+}
+
+export default Settings
